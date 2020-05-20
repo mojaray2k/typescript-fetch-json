ae9abed7173e31c65f983ae135cb8992ac5c6e3c
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-19 20:57:56 -0400

    addedd type annotations to logTodo function

--- index.ts
+++ index.ts
@@ -11,12 +11,16 @@ interface Todo {
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
   const id = todo.id;
-  const Title = todo.title;
+  const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The todo with ID: ${id} 
-    Has a title of: ${Title}
+    Has a title of: ${title}
     Is it completed? ${completed}
   `);
-});
+};
